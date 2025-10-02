06589eb56a82b5d88609ad8c99c9a432ff3e7a92
danielvichi <danielvichi@gmail.com>
2025-10-02 15:00:46 -0300

    Fix Client Module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,12 +2,11 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
-import { ClientController } from './client/client.controller';
 import { ClientModule } from './client/client.module';
 
 @Module({
   imports: [PrismaModule, ClientModule],
-  controllers: [AppController, ClientController],
+  controllers: [AppController],
   providers: [AppService],
 })
 export class AppModule {}
--- src/client/client.module.ts
+++ src/client/client.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ClientService } from './client.service';
 import { PrismaModule } from 'src/prisma/prisma.module';
+import { ClientController } from './client.controller';
 
 @Module({
   imports: [PrismaModule],
+  controllers: [ClientController],
   providers: [ClientService],
   exports: [ClientService],
 })
