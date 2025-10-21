b25ec518850f60fda9282f64292a26b7dfda9ea7
danielvichi <danielvichi@gmail.com>
2025-10-21 18:21:09 -0300

    Reorder `createUnity`

--- src/unity/unity.service.ts
+++ src/unity/unity.service.ts
@@ -26,6 +26,38 @@ export class UnityService {
   constructor(private readonly prisma: PrismaService) {}
 
   /**
+   *  Create a new Unity for a Client in the database using the provided data.
+   *
+   * @param {CreateUnityDto} data  The data required to create a new unity.
+   * @returns {Promise<UnityDto>} The created UnityDto object
+   */
+  async createUnity(data: CreateUnityDto): Promise<UnityDto> {
+    checkCreateUnityRequirementsOrThrowError(data);
+
+    const confirmedClient = await this.prisma.client.findFirst({
+      where: {
+        id: data.clientId,
+      },
+    });
+
+    if (!confirmedClient) {
+      throw new UnityNotFoundException(
+        createUnityBadRequestExceptionMessages.CLIENT_NOT_FOUND,
+      );
+    }
+
+    const newUnity = await this.prisma.unity.create({ data });
+
+    return {
+      ...newUnity,
+      address: newUnity.address ?? undefined,
+      phone: newUnity.phone ?? undefined,
+      email: newUnity.email ?? undefined,
+      createdAt: newUnity.createdAt,
+    };
+  }
+
+  /**
    * Returns all UnityDto for a given Client
    *
    * @param {GetAllUnitiesArgs} data Client Id
@@ -85,38 +117,6 @@ export class UnityService {
     };
 
     return formattedUnity;
-  }
-
-  /**
-   *  Create a new Unity for a Client in the database using the provided data.
-   *
-   * @param {CreateUnityDto} data  The data required to create a new unity.
-   * @returns {Promise<UnityDto>} The created UnityDto object
-   */
-  async createUnity(data: CreateUnityDto): Promise<UnityDto> {
-    checkCreateUnityRequirementsOrThrowError(data);
-
-    const confirmedClient = await this.prisma.client.findFirst({
-      where: {
-        id: data.clientId,
-      },
-    });
-
-    if (!confirmedClient) {
-      throw new UnityNotFoundException(
-        createUnityBadRequestExceptionMessages.CLIENT_NOT_FOUND,
-      );
-    }
-
-    const newUnity = await this.prisma.unity.create({ data });
-
-    return {
-      ...newUnity,
-      address: newUnity.address ?? undefined,
-      phone: newUnity.phone ?? undefined,
-      email: newUnity.email ?? undefined,
-      createdAt: newUnity.createdAt,
-    };
   }
 
   /**
