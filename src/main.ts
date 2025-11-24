3d861f08af0371f132d22b7ba821fd0bc051dc39
danielvichi <danielvichi@gmail.com>
2025-11-24 15:45:31 -0300

    Add CORS

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,12 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { corsOptionsDelegate } from './utils/cors';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors(corsOptionsDelegate);
 
   const config = new DocumentBuilder()
     .setTitle('BE API')
