7c8590eaf59139eec3b37f192b881f2c9c92d1d4
danielvichi <danielvichi@gmail.com>
2025-11-26 12:01:29 -0300

    add cookie-parser module

--- src/main.ts
+++ src/main.ts
@@ -2,11 +2,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { corsOptionsDelegate } from './utils/cors';
+import cookieParser from 'cookie-parser';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors(corsOptionsDelegate);
+  app.use(cookieParser());
 
   const config = new DocumentBuilder()
     .setTitle('BE API')
